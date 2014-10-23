00b2da34bf8facdb84b3822ce68adac8eac9306f
Sarah Simpkin <sarahsimpkin@gmail.com>
2014-10-22 23:34:40 -0400

    Oh hai, now I can insert a random array word.

--- stratplanerator.js
+++ stratplanerator.js
@@ -1,10 +1,13 @@
 
+var phrases = ["QR codes", "innovation", "makerspace", "middle management", "temp jobs"];
 
 function generatePlan() {
 	
 	// message going out to page
-	var msg = 'Library future bullshittery!';
-	
+	// var msg = 'Library future bullshittery!';
+	var msg = phrases[Math.floor(Math.random()*phrases.length)];
+
+
 	// message has to replace the text in the div named "message"
 	var el = document.getElementById('message');
 	el.textContent = msg;
